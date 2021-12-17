ca7de9472907840a9158df81f22af3e37d1c3ab9
ulmalana <muhamaul@gmail.com>
2021-12-17 20:25:20 +0700

    [Rust] COMPLETED - Rustlings 4.6.0

--- rustlings/exercises/advanced_errors/advanced_errs2.rs
+++ rustlings/exercises/advanced_errors/advanced_errs2.rs
@@ -16,7 +16,6 @@
 // 4. Complete the partial implementation of `Display` for
 //    `ParseClimateError`.
 
-// I AM NOT DONE
 
 use std::error::Error;
 use std::fmt::{self, Display, Formatter};
@@ -47,11 +46,16 @@ impl From<ParseIntError> for ParseClimateError {
 impl From<ParseFloatError> for ParseClimateError {
     fn from(e: ParseFloatError) -> Self {
         // TODO: Complete this function
+        //todo!();
+        Self::ParseFloat(e)
     }
 }
 
 // TODO: Implement a missing trait so that `main()` below will compile. It
 // is not necessary to implement any methods inside the missing trait.
+impl Error for ParseClimateError {
+
+}
 
 // The `Display` trait allows for other code to obtain the error formatted
 // as a user-visible string.
@@ -64,6 +68,9 @@ impl Display for ParseClimateError {
         match self {
             NoCity => write!(f, "no city name"),
             ParseFloat(e) => write!(f, "error parsing temperature: {}", e),
+            Empty => write!(f, "empty input"),
+            BadLen => write!(f, "incorrect number of fields"),
+            ParseInt(e) => write!(f, "error parsing year: {}", e),
             _ => write!(f, "unhandled error!"),
         }
     }
@@ -89,14 +96,26 @@ impl FromStr for Climate {
     // TODO: Complete this function by making it handle the missing error
     // cases.
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let v: Vec<_> = s.split(',').collect();
-        let (city, year, temp) = match &v[..] {
-            [city, year, temp] => (city.to_string(), year, temp),
-            _ => return Err(ParseClimateError::BadLen),
-        };
-        let year: u32 = year.parse()?;
-        let temp: f32 = temp.parse()?;
-        Ok(Climate { city, year, temp })
+        if s.len() == 0 {
+            Err(ParseClimateError::Empty)
+        } else {
+            let v: Vec<_> = s.split(',').collect();
+            
+            if v[0].is_empty() {
+                Err(ParseClimateError::NoCity)
+            } else {
+                
+                let (city, year, temp) = match &v[..] {
+                    [city, year, temp] => (city.to_string(), year, temp),
+                    _ => return Err(ParseClimateError::BadLen),
+                };
+                
+                let year: u32 = year.parse()?;
+                let temp: f32 = temp.parse()?;
+                Ok(Climate { city, year, temp })
+            
+            }
+        }
     }
 }
 
--- rustlings/exercises/clippy/target/debug/deps/clippy2-f847b69eaeb18552.d
+++
@@ -1,7 +0,0 @@
-/home/mrmx/Projects/learn_prog/rust/rustlings/exercises/clippy/target/debug/deps/clippy2-f847b69eaeb18552.rmeta: clippy2.rs
-
-/home/mrmx/Projects/learn_prog/rust/rustlings/exercises/clippy/target/debug/deps/clippy2-f847b69eaeb18552.d: clippy2.rs
-
-clippy2.rs:
-
-# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
