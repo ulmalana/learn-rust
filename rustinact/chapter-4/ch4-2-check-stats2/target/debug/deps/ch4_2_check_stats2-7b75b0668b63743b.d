fd45e26f795f08f49212dd38b3f8a2168ffa25e5
Riz Maulana <muhamaul@gmail.com>
2022-01-11 19:39:38 +0700

    [Rust] started chapter 4

---
+++ rustinact/chapter-4/ch4-2-check-stats2/target/debug/deps/ch4_2_check_stats2-7b75b0668b63743b.d
@@ -0,0 +1,5 @@
+/home/maulana/Project/learn_prog/rust/rustinact/chapter-4/ch4-2-check-stats2/target/debug/deps/ch4_2_check_stats2-7b75b0668b63743b.rmeta: src/main.rs
+
+/home/maulana/Project/learn_prog/rust/rustinact/chapter-4/ch4-2-check-stats2/target/debug/deps/ch4_2_check_stats2-7b75b0668b63743b.d: src/main.rs
+
+src/main.rs:
