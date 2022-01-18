4b704ae3c6f3d79ecc47ac0a658224f34d6f82bd
Riz Maulana <muhamaul@gmail.com>
2022-01-18 20:33:55 +0700

    [Rust] started chapter 6

---
+++ rustinact/chapter-6/ch6-2-pointer2/target/debug/ch6-2-pointer2.d
@@ -0,0 +1,1 @@
+/home/maulana/Project/learn_prog/rust/rustinact/chapter-6/ch6-2-pointer2/target/debug/ch6-2-pointer2: /home/maulana/Project/learn_prog/rust/rustinact/chapter-6/ch6-2-pointer2/src/main.rs
---
+++ rustinact/chapter-6/ch6-2-pointer2/target/debug/deps/ch6_2_pointer2-c088dcb82bf8296b.d
@@ -0,0 +1,5 @@
+/home/maulana/Project/learn_prog/rust/rustinact/chapter-6/ch6-2-pointer2/target/debug/deps/ch6_2_pointer2-c088dcb82bf8296b: src/main.rs
+
+/home/maulana/Project/learn_prog/rust/rustinact/chapter-6/ch6-2-pointer2/target/debug/deps/ch6_2_pointer2-c088dcb82bf8296b.d: src/main.rs
+
+src/main.rs:
