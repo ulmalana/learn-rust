7e3b5899f532e4a1c644e19891fcc2fec026a5d6
rz <muhamaul@gmail.com>
2023-09-21 14:35:49 +0700

    finished chapter 6

---
+++ rustinact/chapter-6/memory/examples/listing6-13.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut n_nonzero = 0;
+    for i in 1..10000 {
+        let ptr = i as *const u8;
+        let byte_at_addr = unsafe { *ptr };
+        if byte_at_addr != 0 {
+            n_nonzero += 1;
+        }
+    }
+
+    println!("non-zero bytes in memory: {}", n_nonzero);
+}
