47471fce8607e239b62038cea1a2690d2b809487
RM <muhamaul@gmail.com>
2023-09-30 10:41:11 +0700

    started chapter 10

---
+++ rustinact/chapter-10/threads/src/main.rs
@@ -0,0 +1,20 @@
+use std::{thread, time};
+
+fn main() {
+    let start = time::Instant::now();
+    let handler_1 = thread::spawn(|| {
+        let pause = time::Duration::from_millis(300);
+        thread::sleep(pause.clone());
+    });
+
+    let handler_2 = thread::spawn(|| {
+        let pause = time::Duration::from_millis(300);
+        thread::sleep(pause.clone());
+    });
+
+    handler_1.join().unwrap();
+    handler_2.join().unwrap();
+
+    let finish = time::Instant::now();
+    println!("{:02?}", finish.duration_since(start));
+}
