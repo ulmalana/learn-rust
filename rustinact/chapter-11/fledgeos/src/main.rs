006cc1efbecfbd27fab5cd354a68571068fb3e5f
RM <muhamaul@gmail.com>
2023-10-11 09:46:41 +0700

    finished chapter 11

--- rustinact/chapter-11/fledgeos/src/main.rs
+++ rustinact/chapter-11/fledgeos/src/main.rs
@@ -3,17 +3,92 @@
 #![feature(core_intrinsics)]
 #![feature(lang_items)]
 
+use core::fmt;
+use core::fmt::Write;
 use core::intrinsics;
 use core::panic::PanicInfo;
 use x86_64::instructions::hlt;
 
+#[allow(unused)]
+#[derive(Clone, Copy)]
+#[repr(u8)]
+enum Color {
+    Black = 0x0,
+    White = 0xF,
+    Blue = 0x1,
+    BrightBlue = 0x9,
+    Green = 0x2,
+    BrightGreen = 0xA,
+    Cyan = 0x3,
+    BrightCyan = 0xB,
+    Red = 0x4,
+    BrightRed = 0xC,
+    Magenta = 0x5,
+    BrightMagenta = 0xD,
+    Brown = 0x6,
+    Yellow = 0xE,
+    Gray = 0x7,
+    DarkGray = 0x8,
+}
+
+struct Cursor {
+    position: isize,
+    foreground: Color,
+    background: Color,
+}
+
+impl Cursor {
+    fn color(&self) -> u8 {
+        let fg = self.foreground as u8;
+        let bg = (self.background as u8) << 4;
+        fg | bg
+    }
+
+    fn print(&mut self, text: &[u8]) {
+        let color = self.color();
+        let framebuffer = 0xb8000 as *mut u8;
+        for &character in text {
+            unsafe {
+                framebuffer.offset(self.position).write_volatile(character);
+                framebuffer.offset(self.position + 1).write_volatile(color);
+            }
+            self.position += 2;
+        }
+    }
+}
+
+impl fmt::Write for Cursor {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.print(s.as_bytes());
+        Ok(())
+    }
+}
+
 #[panic_handler]
 #[no_mangle]
-pub fn panic(_info: &PanicInfo) -> ! {
-    unsafe {
-        intrinsics::abort();
+pub fn panic(info: &PanicInfo) -> ! {
+    let mut cursor = Cursor {
+        position: 0,
+        foreground: Color::White,
+        background: Color::Red,
+    };
+
+    for _ in 0..(80 * 25) {
+        cursor.print(b" ");
     }
-    
+
+    cursor.position = 0;
+
+    write!(cursor, "{}", info).unwrap();
+
+    // unsafe {
+    //     intrinsics::abort();
+    // }
+    loop {
+        unsafe {
+            hlt();
+        }
+    }
 }
 
 #[lang = "eh_personality"]
@@ -25,12 +100,20 @@ pub extern "C" fn _start() -> ! {
     let framebuffer = 0xb8000 as *mut u8;
 
     unsafe {
-        framebuffer
-        .offset(1)
-        .write_volatile(0x30);
+        framebuffer.offset(1).write_volatile(0x30);
     }
+    let text = b"Sapere Aude!";
+
+    let mut cursor = Cursor {
+        position: 0,
+        background: Color::Black,
+        foreground: Color::BrightCyan,
+    };
+
+    cursor.print(text);
 
     loop {
         hlt();
     }
+    // panic!("halp!");
 }
